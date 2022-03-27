a6ca25e60b932ffd43d4a1a0bdda7c4fe9218472
pavithramuddu93 <pavithramuddu93>
2022-03-27 12:11:08 +0530

    UC1 added

---
+++ src/com/linkedlist/LinkedList.java
@@ -0,0 +1,31 @@
+package com.linkedlist;
+
+public class LinkedList<T> {
+    Node<T> head;
+
+    public void add(T value) {
+        Node<T> newNode = new Node<T>();
+        newNode.data = value;
+        newNode.next = null;
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> last = head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = newNode;
+        }
+    }
+
+    public void show() {
+        Node<T> node = head;
+        while (node.next != null) {
+            System.out.println(node.data);
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
+
+
+}
---
+++ src/com/linkedlist/Runner.java
@@ -0,0 +1,10 @@
+package com.linkedlist;
+public class Runner {
+    public static void main(String[] args) {
+        LinkedList<Integer> numberList = new LinkedList<>();
+        numberList.add(56);
+        numberList.add(30);
+        numberList.add(70);
+        numberList.show();
+    }
+}
